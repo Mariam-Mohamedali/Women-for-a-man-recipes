3e39da2dd2ead92738a8cf9169d79c0edcd1e60f
Mariam-Mohamedali <mariammohamedali127@gmail.com>
2026-05-13 00:15:18 +0300

    deploy

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-oy&^rj!1zl^5oxkit12!hd^8r=!qe%#a*h%xfeba)zmf+a4y5l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main', 'static'),
 ]
