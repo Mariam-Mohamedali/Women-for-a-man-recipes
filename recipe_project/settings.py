8bc40486a2a1e11819a49c8713779fe1b01e304a
Mariam-Mohamedali <mariammohamedali127@gmail.com>
2026-07-21 02:24:53 +0300

    fix: add CSRF_TRUSTED_ORIGINS for Railway domain

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -21,6 +21,10 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'https://*.up.railway.app'
+).split(',')
 
 # Application definition
 
